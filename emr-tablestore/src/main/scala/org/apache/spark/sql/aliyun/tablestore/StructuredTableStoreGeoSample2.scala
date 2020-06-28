c2ed00cd35de8d0a902f039d25408b9959c41884
zhuoran.wzr <zhuoran.wzr@alibaba-inc.com>
2020-06-28 20:50:50 +0800

    add support for sql schema

--- emr-tablestore/src/main/scala/org/apache/spark/sql/aliyun/tablestore/StructuredTableStoreGeoSample2.scala
+++ emr-tablestore/src/main/scala/org/apache/spark/sql/aliyun/tablestore/StructuredTableStoreGeoSample2.scala
@@ -34,6 +34,7 @@ object StructuredTableStoreGeoSample2 extends Logging {
 
     spark.sparkContext.setLogLevel("INFO")
     spark.read
+      .schema("pk1 STRING, val_long1 STRING, val_geo STRING")
       .format("tablestore")
       .option("instance.name", instanceName)
       .option("table.name", tableName)
@@ -42,23 +43,23 @@ object StructuredTableStoreGeoSample2 extends Logging {
       .option("access.key.id", accessKeyId)
       .option("access.key.secret", accessKeySecret)
       .option("maxOffsetsPerChannel", maxOffsetsPerChannel) // default 10000
-      .option(
-        "catalog",
-        """{"columns": {"pk1": {"type": "string"} , "val_long1": {"type": "long"}, "val_geo": {"type": "string"}
-          |}}""".stripMargin
-      )
+      //      .option(
+      //        "catalog",
+      //        """{"columns": {"pk1": {"type": "string"} , "val_long1": {"type": "long"}, "val_geo": {"type": "string"}
+      //          |}}""".stripMargin
+      //      )
       .option("search.index.name", "geo_table_index")
       .load()
       .createTempView("search_view")
 
     val geoDistanceQuery = spark.sql("""SELECT COUNT(*) FROM search_view WHERE val_geo = '{"centerPoint":"6,9", "distanceInMeter": 10000}' LIMIT 100""")
-//    val geoDistanceQuery = spark.sql("""SELECT COUNT(*) FROM search_view WHERE pk1 = '1420wurpoljjkhjkkigeca99544225' LIMIT 100""")
+    //    val geoDistanceQuery = spark.sql("""SELECT COUNT(*) FROM search_view WHERE pk1 = '1420wurpoljjkhjkkigeca99544225' LIMIT 100""")
     geoDistanceQuery.show()
 
-//    val geoBoundingBoxQuery = spark.sql("""SELECT * FROM search_view WHERE geo = '{"topLeft":"8,0", "bottomRight": "0,10"}' """)
-//    geoBoundingBoxQuery.show()
+    //    val geoBoundingBoxQuery = spark.sql("""SELECT * FROM search_view WHERE geo = '{"topLeft":"8,0", "bottomRight": "0,10"}' """)
+    //    geoBoundingBoxQuery.show()
 
-//    val geoPolygonQuery = spark.sql("""SELECT * FROM search_view WHERE geo = '{"points":["5,0", "5,1", "6,1", "6,10"]}' """)
-//    geoPolygonQuery.show()
+    //    val geoPolygonQuery = spark.sql("""SELECT * FROM search_view WHERE geo = '{"points":["5,0", "5,1", "6,1", "6,10"]}' """)
+    //    geoPolygonQuery.show()
   }
 }
